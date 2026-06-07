26d6ab575d4b33f5d16c22a7a54819336a74540a
IKuraii <zyeead10@gmail.com>
2026-06-07 17:12:39 +0300

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
             
             // الانتقال الفعلي لصفحة اللوبي بعد عرض رسالة الترحيب بـ 2.5 ثانية
             setTimeout(() => {
-                window.location.href = 'lobby.html';
+                window.location.href = 'rooms.html';
             }, 2500); 
 
         }, 1000); 
